041ecc97ad874db393d15a0d270064358646725e
vanilla-terrykerrigan <terrykerrigan428@gmail.com>
2020-10-03 17:06:17 +0900

    re

--- app/app.py
+++ app/app.py
@@ -10,17 +10,17 @@ def home():
 @app.route('/42reboot', methods=['POST'])
 def reboot():
     msg = start_ansible(request, 'reboot')
-    return Response('/42reboot' + msg)
+    return Response('/42reboot  ' + msg)
 
 @app.route('/42default', methods=['POST'])
 def default_mode():
     msg = start_ansible(request, 'default')
-    return Response('/42default' + msg)
+    return Response('/42default  ' + msg)
 
 @app.route('/42exam', methods=['POST'])
 def exam_mode():
     msg = start_ansible(request, 'exam')
-    return Response('/42exam' + msg)
+    return Response('/42exam  ' + msg)
 
 if __name__ == '__main__':
     app.run()
